608fdea478a6245e956dfe721eb5115a38079007
DaVinciWithLaptop <officialgaikwadaryan@gmail.com>
2026-03-16 00:37:16 +0530

    Updated RMSE function

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,7 +4,7 @@ def mean_squared_error(y_true, y_pred):
 	return np.mean((y_true - y_pred)**2)
 
 
-def mean_squared_error(y_true, y_pred):
+def root_mean_squared_error(y_true, y_pred):
 	return np.sqrt(np.mean((y_true - y_pred)**2))
 
 def mean_absolute_error(y_true, y_pred):
